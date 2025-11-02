cf478dd357e43a0300db68f48be1982500119f3e
siyFred <fred87gamer@gmail.com>
2025-11-02 00:19:23 -0300

    feat: model Link criado

---
+++ src/main/java/com/siyfred/urlshortener/model/Link.java
@@ -0,0 +1,67 @@
+package com.siyfred.urlshortener.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "links")
+public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "long_url", nullable = false, columnDefinition = "TEXT")
+    private String longUrl;
+
+    @Column(name = "short_code", nullable = false, unique = true, length = 7)
+    private String shortCode;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Link() {
+    }
+
+    public Link(String longUrl, String shortCode) {
+        this.longUrl = longUrl;
+        this.shortCode = shortCode;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortUrl) {
+        this.shortCode = shortUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
