01a2dd85a45e41a1c59156bcfd6fde362aeb6167
siyFred <fred87gamer@gmail.com>
2025-11-04 01:41:15 -0300

    refactor(service): lógica de UUID substituida por Base62 (agora é previsivel: INSERT e UPDATE)

--- src/main/java/com/siyfred/urlshortener/service/LinkService.java
+++ src/main/java/com/siyfred/urlshortener/service/LinkService.java
@@ -2,33 +2,38 @@ package com.siyfred.urlshortener.service;
 
 import com.siyfred.urlshortener.model.Link;
 import com.siyfred.urlshortener.repository.LinkRepository;
+import com.siyfred.urlshortener.util.Base62;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class LinkService {
     private final LinkRepository linkRepository;
+    private final Base62 base62;
 
-    public LinkService(LinkRepository linkRepository) {
+    public LinkService(LinkRepository linkRepository, Base62 base62) {
         this.linkRepository = linkRepository;
+        this.base62 = base62;
     }
 
     public Optional<Link> getLongUrlByShortCode(String shortCode) {
         return linkRepository.findByShortCode(shortCode);
     }
 
+    @Transactional
     public Link createShortUrl(String longUrl) {
-        // TODO: usar base62 para garantir unicidade com previsibilidade sem precisar consultar a db
         String formattedUrl = formatUrl(longUrl);
-        String shortCode;
-        do {
-            shortCode = UUID.randomUUID().toString().substring(0, 7);
-        } while(linkRepository.findByShortCode(shortCode).isPresent());
 
-        Link link = new Link(formattedUrl, shortCode);
-        return linkRepository.save(link);
+        Link link = new Link(formattedUrl, null);
+        Link savedLink = linkRepository.save(link);
+
+        long id = savedLink.getId();
+        String shortCode = base62.encode(id);
+        savedLink.setShortCode(shortCode);
+        
+        return linkRepository.save(savedLink);
     }
 
     private String formatUrl(String url) {
